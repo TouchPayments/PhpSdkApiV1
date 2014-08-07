ed42d4a6a969b0ba349ba39dd3e0a0b4ba45bef4
vmignot <vmignot@gmail.com>
2014-08-07 12:58:27 +1000

    better documentation and move default value to attribute for clarity

--- Touch/Address.php
+++ Touch/Address.php
@@ -1,63 +1,64 @@
 <?php
+
 /**
  * Touch Payments Address Object Wrapper
- * 
+ *
  * @copyright 2013 Check'n Pay Finance Pty Limited
  */
-class Touch_Address extends Touch_Object {
+class Touch_Address extends Touch_Object
+{
 
+    /**
+     * Australia country code
+     */
     const COUNTRY_AU = 'au';
 
-     /**
-     * @var String
+    /**
+     * @var string first name
      */
     public $firstName;
     /**
-     * @var String
+     * @var string last name
      */
     public $lastName;
     /**
-     * @var String
+     * @var string middle name
      */
     public $middleName;
     /**
-     * @var String
+     * @var string number on the street
      */
     public $number;
 
     /**
-     * @var String
+     * @var string first line of the address
      */
     public $addressOne;
 
     /**
-     * @var String
+     * @var string second line of the address
      */
     public $addressTwo;
 
     /**
-     * @var String
+     * @var string postcode
      */
     public $postcode;
 
     /**
-     * @var String
+     * @var string suburb
      */
     public $suburb;
 
     /**
-     * @var String
+     * @var string state
      */
     public $state;
 
     /**
-     * @var String
+     * @var string country
+     * Default is Australia at the moment
      */
-    public $country;
-
-    public function __construct($country = self::COUNTRY_AU)
-    {
-        $this->country = $country;
-    }
+    public $country = self::COUNTRY_AU;
 
 }
